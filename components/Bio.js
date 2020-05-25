6cb1f4361cc81fb3fb77e0cef957cd6adbf73542
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-05-24 22:20:17 -0600

    landing page layout complete for desktop

--- components/Bio.js
+++ components/Bio.js
@@ -1,7 +1,35 @@
+import img from '../public/img/profile.jpg';
+
 export default function Bio() {
   return (
     <div className="content">
-      
+      <h2 className="title">Bryant Vaughn</h2>
+      <img className="image" src={img} />
+      <h5 className="sub-text">Front-end Developer</h5>
+    
+      <style jsx>{`
+        .content {
+          display: block;
+          text-align: center;
+          padding-top: 10%;
+          text-transform: uppercase;
+          letter-spacing: 0.15rem;
+          color: #fff;
+        }
+
+        .sub-text {
+          font-weight: 200;
+          margin: 0.5rem;
+        }
+
+        .image {
+          border-radius: 50%;
+          height: 260px;
+          width: 260px;
+          box-shadow: 0 0 40px #fff;
+          margin: 2.5rem;
+        }
+    `}</style>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Bio from '../components/Bio';
-import img from '../public/img/campnou.jpg';
+import img from '../public/img/denver.jpg';
 
 export default function Home() {
   return (
@@ -8,7 +8,25 @@ export default function Home() {
       <Head>
         <title>Bryant Vaughn</title>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossOrigin="anonymous" />
+        <script
+          src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+          integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+          crossorigin="anonymous"></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+          integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+          crossorigin="anonymous"></script>
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+          crossOrigin="anonymous"
+        />
+        <script
+          src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+          integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+          crossorigin="anonymous"></script>
+
       </Head>
 
       <main>
@@ -24,7 +42,6 @@ export default function Home() {
         </nav>
         <div className="about">
           <Bio />
-
         </div>
         <div id="section-two"></div>
         <div id="section-three"></div>
@@ -34,13 +51,13 @@ export default function Home() {
       <style jsx>{`
         .nav-container {
           width: 50%;
-          height: 25px;
+          height: 50px;
           margin: auto;
           display: flex;
           justify-content: space-between;
           align-items: center;
           text-transform: uppercase;
-          letter-spacing: 0.1rem;
+          letter-spacing: 0.15rem;
           background-color: transparent;
         }
         .nav-container a {
@@ -48,18 +65,24 @@ export default function Home() {
           color: #fff;
         }
 
+        #toggle {
+          color: #fff;
+        }
+
         .nav-items {
           display: flex;
-          width: 50%;
+          width: 40%;
           justify-content: space-between;
         }
         .nav-items a {
           text-decoration: none;
-          color: rgba(255,255,255,0.6);
+          color: rgba(255,255,255,1.0);
           transition: 0.3s ease-in-out;
+          font-size: small;
+          font-weight: 300;
         }
         .nav-items a:hover {
-          color: rgba(219,133,21,1.0);
+          color: rgba(255,255,255,0.6);
         }
 
         .page-content {
