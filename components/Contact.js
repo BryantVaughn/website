e8c7328ef7fb82827df8d8472687373c29c4e5f3
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-05-29 23:39:15 -0600

    contact footer details

--- components/Contact.js
+++ components/Contact.js
@@ -1,3 +1,7 @@
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
 import styles from './Contact.module.css';
 
 export default function Contact() {
@@ -7,11 +11,50 @@ export default function Contact() {
         <h3 className={styles.title}>Contact</h3>
         <p>Shoot me an email or check out my online profiles</p>
         <a
+          className={styles.email}
           href="mailto:bf_vaughn@hotmail.com"
           target="_blank"
         >
           bf_vaughn@hotmail.com
         </a>
+        <section className={styles.icons}>
+          <a
+            href="https://www.linkedin.com/in/bryant-vaughn-818950142/"
+            target="_blank"
+          >
+            <LinkedInIcon
+              style={{ fontSize: 40 }}
+            />
+          </a>
+          <a
+            href="https://github.com/BryantVaughn"
+            target="_blank"
+          >
+            <GitHubIcon
+              style={{ fontSize: 40 }}
+            />
+          </a>
+          <a
+            href="https://www.instagram.com/bf_vaughn/"
+            target="_blank"
+          >
+            <InstagramIcon
+              style={{ fontSize: 40 }}
+            />
+          </a>
+          <a
+            href="https://www.facebook.com/bryant.vaughn/"
+            target="_blank"
+          >
+            <FacebookIcon
+              style={{ fontSize: 40 }}
+            />
+          </a>
+        </section>
+        <p className={styles.website}>This website designed and developed by Bryant Vaughn: 
+          <a href="https://github.com/BryantVaughn/website" target="_blank"> View it on Github</a>
+        </p>
+        <p>&copy; Bryant Vaughn 2020</p>
       </div>
     </div>
   );
