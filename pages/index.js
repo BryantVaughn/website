9d701a8897aa715b029385cee3befff24f24d454
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-07-19 21:23:10 -0600

    reduce image size and add min height

--- pages/index.js
+++ pages/index.js
@@ -8,67 +8,65 @@ import denverImg from '../public/img/denver.jpg';
 import resumeImg from '../public/img/resumeImg.jpg';
 
 export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>Bryant Vaughn</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-  return (
-    <>
-      <Head>
-        <title>Bryant Vaughn</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+			<main>
+				<div className="about" id="about">
+					<Navbar />
+					<Bio />
+					<div className="button"></div>
+				</div>
+				<div className="resume" id="resume">
+					<Resume />
+				</div>
+				<div id="experience">
+					<Experience />
+				</div>
+				<div className="contact" id="contact">
+					<Contact />
+				</div>
+			</main>
 
-      <main>
-        <div className="about" id="about">
-          <Navbar />
-          <Bio />
-          <div className="button">
-            
-          </div>
-        </div>
-        <div className="resume" id="resume">
-          <Resume />
-        </div>
-        <div id="experience">
-          <Experience />
-        </div>
-        <div className="contact" id="contact">
-          <Contact />
-        </div>
-      </main>
+			<style jsx>{`
+				.title {
+					font-size: 22px;
+				}
 
-      <style jsx>{`
-        .title {
-          font-size: 22px;
-        }
+				.about {
+					background-image: url(${denverImg});
+					height: 100vh;
+					min-height: 768px;
+					width: 100%;
+					background-repeat: no-repeat;
+					background-size: cover;
+				}
 
-        .about {
-          background-image: url(${denverImg});
-          height: 100vh;
-          width: 100%;
-          background-repeat: no-repeat;
-          background-size: cover;
-        }
+				.resume {
+					background-image: url(${resumeImg});
+					height: 50vh;
+					width: 100%;
+					background-repeat: no-repeat;
+					background-size: cover;
+					background-attachment: fixed;
+					z-index: -10;
+				}
+			`}</style>
 
-        .resume {
-          background-image: url(${resumeImg});
-          height: 50vh;
-          width: 100%;
-          background-repeat: no-repeat;
-          background-size: cover;
-          background-attachment: fixed;
-          z-index: -10;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-      `}</style>
-    </>
-  );
+			<style jsx global>{`
+				html,
+				body {
+					padding: 0;
+					margin: 0;
+					font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+						Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+						sans-serif;
+				}
+			`}</style>
+		</>
+	);
 }
